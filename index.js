eb215acc22a641dd240ceddc8da0c127e0e23d19
Mojammel-1361 <mojammel1361@gmail.com>
2022-11-09 14:10:53 +0600

    quary paramiter done

--- index.js
+++ index.js
@@ -28,7 +28,18 @@ async function run(){
         const serviceCollection = client.db('Doctor').collection('services');
 
         const reviewCollection = client.db("Doctor").collection("reviews");
-        
+        app.get('/reviews', async(req, res) =>{
+            let query = {};
+            if(req.query.email){
+                query = {
+                  email: req.query.email
+                };
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+
+        })
         app.post("/reviews", async (req, res) => {
           const review = req.body;
           const result = await reviewCollection.insertOne(review);
